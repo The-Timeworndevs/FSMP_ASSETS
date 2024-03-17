24e2151388a85f3b82a6a10fe0f8979966c24790
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-03-17 10:41:15 -0400

    Don't require Dynamic Doors blocks to be initialized to load client-side

--- src/client/java/net/timeworndevs/figcorpclient/Client.java
+++ src/client/java/net/timeworndevs/figcorpclient/Client.java
@@ -2,12 +2,24 @@ package net.timeworndevs.figcorpclient;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
 import net.timeworndevs.figcorpmain.common.CommonBlockRegistry;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		List<@NotNull Block> translucentBlocks = new ArrayList<>(4);
+		translucentBlocks.add(CommonBlockRegistry.SRF_BLAST_DOOR);
+		if (CommonBlockRegistry.TALL_SRF_BLAST_DOOR != null)
+			translucentBlocks.add(CommonBlockRegistry.TALL_SRF_BLAST_DOOR);
+		translucentBlocks.add(CommonBlockRegistry.SRF_DOOR);
+		if (CommonBlockRegistry.TALL_SRF_DOOR != null)
+			translucentBlocks.add(CommonBlockRegistry.TALL_SRF_DOOR);
 		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(), CommonBlockRegistry.SRF_BLAST_DOOR, CommonBlockRegistry.TALL_SRF_BLAST_DOOR, CommonBlockRegistry.SRF_DOOR, CommonBlockRegistry.TALL_SRF_DOOR);
 	}
 }
